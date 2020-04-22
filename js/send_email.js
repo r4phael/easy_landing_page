3489ed52174927d3cefb0266fcfcecafc41ff33b
Jairo Souza <pto.raphael@gmail.com>
2020-04-22 04:00:59 -0300

    Google Analytics

---
+++ js/send_email.js
@@ -0,0 +1,11 @@
+<script>
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbynSJT2eufszo4nJKZUF4vQAux4BcGklVn6jXzk5dah1ayT8Xdy/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+</script>
